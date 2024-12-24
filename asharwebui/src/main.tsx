6dbf8303c210f2a320b2efefcab1f015f97dab2d
Syed Ashar <syedashar25@gmail.com>
2024-12-24 14:37:15 -0600

    added github commit calendar

--- asharwebui/src/main.tsx
+++ asharwebui/src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {MantineProvider} from "@mantine/core";
-import '@mantine/core/styles.css';
 import {BrowserRouter} from "react-router-dom";
+
+import '@mantine/core/styles.css'; // applying mantine ui global styles
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
