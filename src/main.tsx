124e5aad884db826b10d9031bfa2c7de632fdcbf
Syed Ashar <syedashar25@gmail.com>
2025-11-19 21:13:33 -0600

    changin from browser router to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { MantineProvider } from "@mantine/core";
-import { HashRouter, HashRouterh } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 
 import '@mantine/core/styles.css'; // applying mantine ui global styles
 
